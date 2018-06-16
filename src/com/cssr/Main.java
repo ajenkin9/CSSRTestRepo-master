db579686a45e3f372010848337f6e756091d0bb4
Ray3 <ray3@github.com>
2018-06-16 07:32:41 -0500

    ray update

---
+++ src/com/cssr/Main.java
@@ -0,0 +1,10 @@
+package com.cssr;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Hi, Im Ray!");
+    }
+}
