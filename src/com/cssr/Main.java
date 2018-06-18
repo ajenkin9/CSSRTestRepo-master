5cdc0b4bf97887beda09d5860dd53ba9307d3309
Ray3 <ray3@github.com>
2018-06-18 08:28:50 -0500

    hi lee-roy

--- src/com/cssr/Main.java
+++ src/com/cssr/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World!\n");
         System.out.println("Hi, Im Ray!");
         System.out.println("Hey, CSSR team help me write some code.");
+        System.out.println("Hi,Lee-Roy.");
     }
 }
