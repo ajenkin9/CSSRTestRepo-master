7f5edeacf879afb3e604997a2fa75c6a4daf742f
ajenkin9 <32344268+ajenkin9@users.noreply.github.com>
2018-06-19 11:14:26 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+package com.cssr;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("Hi, Im Ray!");
+        System.out.println("Hey, CSSR team help me write some code.");
+        System.out.println("hi");
+        System.out.println("whats up");
+        System.out.println("YEEEERRRRRRR!");
+        System.out.println("WUS POPPIN JIMBO");
+        System.out.println("My name is Thanh***");
+        System.out.println("JoQui$e, we really live in a society.....");
+        System.out.println("BLATTATTATTTBLAATTATATATATTT");
+        System.out.println("\nHi, Is Ray!");
+    }
+}
